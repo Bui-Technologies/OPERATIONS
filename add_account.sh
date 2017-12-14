8800de245816a27888def4db38f013bead0af408
Wilson Bui <Davoletows@users.noreply.github.com>
2017-12-14 09:47:01 -0500

    First operational commit
    Committing the "add_account.sh" shell script to manage administrative user access to Bastion servers...

---
+++ add_account.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# ---------------------------------------------------------------#
+# Wilson Bui 													 #
+# The "Add Account" shell script								 #
+# Version 1.0 - 12/14/2017
+# This script provisions administrative users to Bastion Servers #
+# ---------------------------------------------------------------#
+
+# Version 0.1 - April 4, 2016 - Chandra
+#sh add_account.sh [UNAME] "ssh-rsa [KEY]"
+
+function bail()
+{
+  local msg="${1}"
+  echo "***: ${msg}"
+  exit 1
+}
+
+function bailOnError()
+{
+  local rc="$1"
+  local msg="$2"
+  if [ "$rc" != "0" ] ; then
+    bail "${msg} Error code = $rc"
+  fi
+}
+
+if [ "$#" != "2" ] ; then
+  bail "Usage: $0 <acctname> <sshkey>"
+fi
+
+u="$1"
+k="$2"
+
+base="/home/${u}"
+sshdir="${base}/.ssh"
+sshfile="${sshdir}/authorized_keys"
+
+if [ -d "${base}" ] ; then
+  bail "Directory ${base} already exists!"
+fi
+
+useradd -m "$u"
+bailOnError "$?" "Failed to useradd ${u}."
+
+mkdir "${sshdir}"
+bailOnError "$?" "Failed to mkdir ${sshdir}."
+
+echo "$k" > "${sshfile}"
+bailOnError "$?" "Failed to create ${sshfile}."
+
+chmod 400 ${sshfile}
+bailOnError "$?" "Failed to chmod 400 ${sshfile}."
+
+chmod 700 ${sshdir}
+bailOnError "$?" "Failed to chmod 700 ${sshdir}."
+
+chown -R "${u}:${u}" "${sshdir}"
+bailOnError "$?" "Failed to chown -R ${sshdir}."
+
+echo "> Done"
+echo
+ls -alR "${base}"
+echo
+exit 0
